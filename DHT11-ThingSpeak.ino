1426fad0cb3fdb08c43650816fb9d0ec3085f31b
Alejandro Vivas <alejandrovivash5@gmail.com>
2025-01-11 13:54:37 -0400

    Adding 3 variables

--- DHT11-ThingSpeak.ino
+++ DHT11-ThingSpeak.ino
@@ -9,141 +9,106 @@
 #include <Adafruit_SSD1306.h>
 #include <ThingSpeak.h>
 
-const char* ssid = "MARTE3";   // network SSID (name) 
-const char* password = "carabobo1972";   // network password
+const char* ssid = "MARTE3";
+const char* password = "carabobo1972";
 
-WiFiClient  client;
+WiFiClient client;
 
 unsigned long ChannelID = 2805648;
-const char * myWriteAPIKey = "W85UCBNM4G7YHUXL";
+const char* myWriteAPIKey = "W85UCBNM4G7YHUXL";
 
-// Timer variables
 unsigned long lastTime = 0;
 unsigned long timerDelay = 30000;
 
+#define DHTPIN 4
+#define DHTTYPE DHT11
 
-#define DHTPIN 4     // Digital pin connected to the DHT sensor
-// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
-// Pin 15 can work but DHT must be disconnected during program upload.
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
-// Uncomment whatever type you're using!
-#define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-
-// Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println(F("DHTxx test!"));
-
-if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-  Serial.println(F("SSD1306 allocation failed"));
-  for(;;);
+  Serial.begin(9600);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
   }
 
   dht.begin();
+  WiFi.mode(WIFI_STA);
+  ThingSpeak.begin(client);
 
-  WiFi.mode(WIFI_STA);   
-  
-  ThingSpeak.begin(client);  // Initialize ThingSpeak
-  
-  display.invertDisplay(true);
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 10);
-  display.println("Iniatializing...");
+  display.println("Initializing...");
   display.display();
   delay(2000);
 }
 
 void loop() {
   if ((millis() - lastTime) > timerDelay) {
-    
-    // Connect or reconnect to WiFi
-    if(WiFi.status() != WL_CONNECTED){
-      Serial.print("Attempting to connect");
-      while(WiFi.status() != WL_CONNECTED){
-        WiFi.begin(ssid, password); 
-        delay(5000);     
-      } 
-      Serial.println("\nConnected.");
+    if (WiFi.status() != WL_CONNECTED) {
+      WiFi.begin(ssid, password);
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+      }
+      Serial.println("\nWiFi connected.");
     }
 
-    // Wait a few seconds between measurements.
-    delay(2000);
-
-    // Reading temperature or humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
     float h = dht.readHumidity();
-    // Read temperature as Celsius (the default)
     float t = dht.readTemperature();
-
-    // Check if any reads failed and exit early (to try again).
     if (isnan(h) || isnan(t)) {
       Serial.println(F("Failed to read from DHT sensor!"));
+      display.clearDisplay();
       display.setCursor(0, 0);
       display.println("Sensor Error!");
       display.display();
       return;
     }
 
-    // Compute heat index in Celsius (isFahreheit = false)
     float hic = dht.computeHeatIndex(t, h, false);
 
+
     // Display data on OLED
-    display.cp437(true); // To display the º symbol, use Code Page 437 font
     display.clearDisplay();
-    display.setCursor(0, 15);
+    display.setCursor(0, 0);
     display.print("Humidity: ");
     display.print(h);
     display.println(" %");
+    display.setCursor(0, 10);
     display.print("Temp: ");
     display.print(t);
-    display.print(" ");
-    display.write(167);
-    display.println("C");
+    display.println(" C");
+    display.setCursor(0, 20);
     display.print("Heat Index: ");
     display.print(hic);
-    display.print(" ");
-    display.write(167);
-    display.println("C");
+    display.println(" C");
     display.display();
 
+    // Write to ThingSpeak
 
-        // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
-    // pieces of information in a channel.  Here, we write to field 1.
-    int x = ThingSpeak.writeField(ChannelID, 1, t, myWriteAPIKey);
-    //uncomment if you want to get temperature in Fahrenheit
-    //int x = ThingSpeak.writeField(myChannelNumber, 1, temperatureF, myWriteAPIKey);
+    ThingSpeak.setField(1, t);
+    ThingSpeak.setField(2, h);
+    ThingSpeak.setField(3, hic);
 
-    if(x == 200){
-      Serial.println("Channel update successful.");
+    if (ThingSpeak.writeFields(ChannelID, myWriteAPIKey)) {
+      Serial.println("Data sent to ThingSpeak");
+      display.setCursor(0, 30);
+      display.println("Sent to ThingSpeak");
+      display.display();
+    } else {
+      Serial.println("Failed to send data");
+      display.setCursor(0, 30);
+      display.println("Failed to send data");   
+      display.display();   
     }
-    else{
-      Serial.println("Problem updating channel. HTTP error code " + String(x));
-    }
+
     lastTime = millis();
   }
-
-    // Update the timer
-//lastTime = millis();
-  
 }
+
